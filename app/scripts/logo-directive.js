a3c913e518301636b29c3a83397fbd781fdc2189
Jeff <jeff.blaisdell@gmail.com>
2014-06-27 15:46:14 -0500

    Animating dots under logo.

--- app/scripts/logo-directive.js
+++ app/scripts/logo-directive.js
@@ -1,30 +1,55 @@
-(function() {
+(function () {
     'use strict';
 
     var module = angular.module('jeff-blaisdell.components.logo', [
     ]);
 
-    module.directive('logo', function($timeout) {
+    module.directive('logo', function ($timeout) {
         return {
             restrict: 'E',
-            template: '<div class="logo-container"><img class="logo logo-1-1" src="images/jb-logo.svg" height="200" width="200"><img class="logo logo-1-2" src="images/jb-logo.svg" height="200" width="200"><img class="logo logo-1-3" src="images/jb-logo.svg" height="200" width="200"></div>',
+            template: '<div class="logo-container">' +
+                          '<img class="logo logo-1-1" src="images/jb-logo.svg" height="200" width="200">' +
+                          '<img class="logo logo-1-2" src="images/jb-logo.svg" height="200" width="200">' +
+                          '<img class="logo logo-1-3" src="images/jb-logo.svg" height="200" width="200">' +
+                          '<img class="logo-dot logo-dot-1-3" src="images/jb-logo-dot.svg" height="200" width="200">' +
+                          '<img class="logo-dot logo-dot-2-3" src="images/jb-logo-dot.svg" height="200" width="200">' +
+                          '<img class="logo-dot logo-dot-3-3" src="images/jb-logo-dot.svg" height="200" width="200">' +
+                      '</div>',
             scope: {
             },
-            link: function (scope) {
+            link: function () {
 
-              function animate() {
-                move('.logo-container .logo-1-3')
-                  .x(125)
-                  .rotate(180)
-                  .end();
+                function animateDots() {
+                    move('.logo-dot-2-3')
+                        .ease('in')
+                        .x(55)
+                        .end(function () {
+                        });
 
-                move('.logo-container .logo-1-2')
-                  .x(125)
-                  .scaleX(-1)
-                  .end();
-              }
+                    move('.logo-dot-3-3')
+                        .ease('in')
+                        .x(55)
+                        .end(function () {
+                            move('.logo-dot-3-3')
+                                .ease('in')
+                                .x(110)
+                                .end();
+                        });
+                }
 
-              $timeout(animate, 1500);
+                function animate() {
+                    move('.logo-container .logo-1-3')
+                        .x(125)
+                        .rotate(180)
+                        .end();
+
+                    move('.logo-container .logo-1-2')
+                        .x(125)
+                        .scaleX(-1)
+                        .end(animateDots);
+                }
+
+                $timeout(animate, 1500);
 
             }
         }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
-(function() {
+(function () {
     'use strict';
 
     var module = angular.module('jeff-blaisdell.pages.main.app', [
-      'jeff-blaisdell.components.logo',
-      'jeff-blaisdell.components.parallax'
+        'jeff-blaisdell.components.logo',
+        'jeff-blaisdell.components.parallax'
     ]);
 
-    module.controller('MainCtrl', function($scope, $log) {
+    module.controller('MainCtrl', function ($scope, $log) {
         $log.info('Loading Main Controller.')
     });
 
